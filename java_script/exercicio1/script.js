3d1d6a26ddf3ca2c25fbaf5b2658c4b258acd376
gAbriel375444 <00001122947975sp@al.educacao.sp.gov.br>
2026-04-07 11:54:29 -0300

    Meus primeiros códigos em JS
    Criei uma pasta pras aulas de java script e um para os exercícios.
    
    Depois criei um html e linkei o java lá.
    
    depois criei um botão
    
    o resto dos detalhes tá no código.

---
+++ java_script/exercicio1/script.js
@@ -0,0 +1,12 @@
+//pegando o id do botão no html
+let botao = document.getElementById("botao");
+
+//adicionando um evento a função botão
+//digo a função começa quando dou um click
+botao.addEventListener("click", function ()
+{
+    //o alert funciona como um show message no game maker
+    //ou seja, cria um popap
+    alert("O botão foi clicado");
+
+});

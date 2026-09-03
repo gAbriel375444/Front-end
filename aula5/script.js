b2d11e17b26e179a60c4d76feea0605a9d770d23
gAbriel375444 <00001122947975sp@al.educacao.sp.gov.br>
2026-09-03 11:24:46 -0300

    .

--- aula5/script.js
+++ aula5/script.js
@@ -1,2 +1,7 @@
-document.getElementById('btn').addEventListener('click', ()=>{
-    document.getElementById('card'). classlist.add('entrar')})
+// document.getElementById('btn').addEventListener('click', ()=>{
+//     document.getElementById('card'). classlist.add('entrar')})
+
+bnt = document.getElementById('btn')
+
+bnt.addEventListener('click', ()=>{
+    document.getElementById('card').classlist.add('entrar')})
